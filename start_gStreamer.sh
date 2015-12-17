13230763ad6ca267fb8077412acb85bdc3b9ca83
Benjamin Nold <brnold@gmail.com>
2015-12-17 18:58:45 +0000

    first commit

---
+++ start_gStreamer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+raspivid -t 0 -h 720 -w 1080 -fps 25 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=10.0.1.15 port=5004
