2548dd3c5654c6ead63dc2fba89ec564d43fdb7e
Benjamin Nold <brnold@oakland.edu>
2015-12-17 21:25:49 +0000

    Working script

--- start_gStreamer.sh
+++ start_gStreamer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-raspivid -t 0 -h 720 -w 1080 -fps 25 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=10.0.1.15 port=5004
+raspivid -t 999999 -w 1920 -h 1080 -fps 30 -rot 180 -b 8000000 -o - | gst-launch-1.0 -e -vvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=10.0.1.15 port=5004
